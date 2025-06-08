9cad3c7ce25475fe5f3885d230a044c546e8bb9f
dogth <dogth@kitteth.com>
2025-06-08 12:48:51 +0300

    docs: add documentation

--- include/ContainerMacros.hpp
+++ include/ContainerMacros.hpp
@@ -1,10 +1,24 @@
+/** @file ContainerMacros.hpp
+ * @brief Макросы для работы с контейнерами и сервисами
+ * @version 1.0
+ *
+ * Этот файл содержит макросы, используемые для регистрации сервисов и создания
+ * фабрик. Макросы позволяют легко управлять количеством аргументов и создавать
+ * шаблоны кода.
+ */
+
 #ifndef CONTAINER_MACROS_HPP
 #define CONTAINER_MACROS_HPP
 
 // Tuple expansion macro
-#define EXPAND(...) __VA_ARGS__
+#define EXPAND(...) __VA_ARGS__ // Макрос для разворачивания аргументов
 
 // ------ Service Macros ------
+/** @brief Макрос для регистрации сервисов с различным количеством аргументов
+ * @details Этот макрос используется для создания шаблонов функций регистрации
+ * сервисов с различным количеством аргументов. Он позволяет легко добавлять
+ * новые сервисы с разным количеством параметров.
+ */
 #define R_ARITY_LIST(X)                                                        \
   X(1, (typename A1), (A1 arg1), (A1), (arg1))                                 \
   X(2, (typename A1, typename A2), (A1 arg1, A2 arg2), (A1, A2), (arg1, arg2)) \
@@ -33,6 +47,19 @@
     (A1, A2, A3, A4, A5, A6, A7, A8),                                          \
     (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8))
 
+/** @brief Макрос для регистрации сервисов с различным количеством аргументов
+ * @details Этот макрос используется для создания шаблонов функций регистрации
+ * сервисов с различным количеством аргументов. Он позволяет легко добавлять
+ * новые сервисы с разным количеством параметров.
+ * @param N Номер арности
+ * @param TMPL Шаблонные параметры
+ * @param FUNC Функция регистрации
+ * @param TPS Типы параметров
+ * @param ARGS Аргументы для конструктора фабрики
+ *
+ * @note Расширение метода регистрации из @link Container::registerService для
+ * различного количества аргументов.
+ */
 #define R_GEN(N, TMPL, FUNC, TPS, ARGS)                                        \
   template <typename T, EXPAND TMPL>                                           \
   bool registerService(Strategy strategy, EXPAND FUNC) {                       \
@@ -47,10 +74,16 @@
     return addService<T>(strategy, factory);                                   \
   }
 
-#define REGISTER_GEN R_ARITY_LIST(R_GEN)
+#define REGISTER_GEN                                                           \
+  R_ARITY_LIST(R_GEN) // Макрос для генерации функций регистрации сервисов с
+                      // различной арностью
 
 // ------- Factory Macros -------
-// Arity list can(and probably should) be redefined
+/** @brief Макросы для создания фабрик с различным количеством аргументов
+ * @details Эти макросы используются для создания фабрик, которые могут
+ * создавать объекты с различным количеством аргументов. Они позволяют легко
+ * управлять количеством параметров и создавать шаблоны кода.
+ */
 #define F_ARITY_LIST(X)                                                        \
   X(1, (typename A1), (A1 _arg1), (A1 arg1), (_arg1(arg1)), (_arg1))           \
   X(2, (typename A1, typename A2), (A1 _arg1; A2 _arg2), (A1 arg1, A2 arg2),   \
@@ -94,6 +127,20 @@
      _arg6(arg6), _arg7(arg7), _arg8(arg8)),                                   \
     (_arg1, _arg2, _arg3, _arg4, _arg5, _arg6, _arg7, _arg8))
 
+/** @brief Макрос для создания фабрик с различным количеством аргументов
+ * @details Этот макрос используется для создания шаблонов фабрик, которые могут
+ * создавать объекты с различным количеством аргументов. Он позволяет легко
+ * управлять количеством параметров и создавать шаблоны кода.
+ * @param N Номер арности
+ * @param TMPL Шаблонные параметры
+ * @param FIELDS Поля класса фабрики
+ * @param ARGS Аргументы конструктора фабрики
+ * @param CONSTR Конструктор фабрики
+ * @param CREATE Создание объекта
+ *
+ * @note Расширение метода создания фабрики для различного количества
+ * аргументов. @link Factory::create
+ */
 #define F_GEN(N, TMPL, FIELDS, ARGS, CONSTR, CREATE)                           \
   template <typename T, EXPAND TMPL> class Factory##N : public IFactory {      \
     EXPAND FIELDS;                                                             \
@@ -109,6 +156,7 @@
     }                                                                          \
   };
 
-#define FACTORY_GEN F_ARITY_LIST(F_GEN)
+#define FACTORY_GEN                                                            \
+  F_ARITY_LIST(F_GEN) // Макрос для генерации фабрик с различной арностью
 
 #endif // CONTAINER_MACROS_HPP
--- include/Descriptor.hpp
+++ include/Descriptor.hpp
@@ -1,3 +1,12 @@
+/** @file Descriptor.hpp
+ * @brief Заголовочный файл для структуры Descriptor. Структура предназначена
+ * для хранения информации о сервисах, включая фабрику, стратегию создания и
+ * экземпляр.
+ * @version 1.0
+ *
+ * Этот файл содержит определение структуры Descriptor, которая используется для
+ * хранения информации о сервисах в контейнере Knot.
+ */
 #ifndef DESCRIPTOR_HPP
 #define DESCRIPTOR_HPP
 
@@ -5,17 +14,23 @@
 #include "Strategy.hpp"
 
 namespace Knot {
+/** @brief Структура Descriptor для хранения информации о сервисах
+ * @details Эта структура используется для хранения информации о сервисах,
+ * включая фабрику, стратегию создания, экземпляр и хранилище.
+ */
 struct Descriptor {
-  IFactory *factory;
-  Strategy strategy;
-  void *instance;
-  void *storage;
+  IFactory *factory; // Указатель на фабрику, которая создает экземпляры сервиса
+  Strategy strategy; // Стратегия создания сервиса (SINGLETON или TRANSIENT)
+  void *instance; // Указатель на экземпляр сервиса, если он создан. Применяется
+                  // только для SINGLETON
+  void *storage;  // Указатель на хранилище, где хранится сервис. Используется
+                  // для SINGLETON сервисов
 
-  Descriptor() : factory(0), strategy(SINGLETON), instance(0), storage(0) {}
+  Descriptor() : factory(0), strategy(), instance(0), storage(0) {}
 
 private:
-  Descriptor(const Descriptor &);
-  Descriptor &operator=(const Descriptor &);
+  Descriptor &operator=(const Descriptor &); // Запрет присваивания дескриптора
+  Descriptor(const Descriptor &);            // Запрет копирования дескриптора
 };
 }; // namespace Knot
 
